02a54261d1668f67c8ae0e42304c12bee32ac65f
sainudheenp <sainudheen2003@gmail.com>
2025-03-25 18:11:03 +0530

    add user and time routes; format urlencoded middleware

--- server/app.js
+++ server/app.js
@@ -3,7 +3,13 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+
+//routes
+// app.use('/',)
+app.use('/',)
+
 
 
 module.exports = app;
