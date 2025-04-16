7893ab1240fa0b66d94aa5dde9e2249153a919a9
sainudheenp <sainudheen2003@gmail.com>
2025-04-16 19:09:07 +0530

    Refactor Login component to use state for email and password inputs

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { auth } from "../../firebase/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { signInWithPopup } from "firebase/auth";
@@ -6,22 +6,45 @@ import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
     try {
       const userCred = await signInWithEmailAndPassword(
         auth,
-        "john@gmail.com",
-        "john@123"
+        email,
+        password
+        // "john@gmail.com",
+        // "john@123"
       );
       const token = await userCred.user.getIdToken();
       localStorage.setItem("token", token);
+      console.log("Sign in User:", userCred);
+      console.log("UID :", userCred.user.uid);
       navigate("/");
     } catch (error) {
       console.log(error);
     }
   };
-  return <button onClick={handleLogin}>Log In</button>;
+  return (
+    <div >
+      <h1>Login</h1>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button onClick={handleLogin}>Log In</button>
+    </div>
+  );
 };
 
 export default Login;
--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -45,6 +45,8 @@ const Register = () => {
 
   return (
     <div>
+      <h1>SignUp</h1>
+
       <input
         type="email"
         value={email}
