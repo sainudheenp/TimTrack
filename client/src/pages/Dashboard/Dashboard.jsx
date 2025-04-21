e8d2884aaf39fc3e69cc16890281541fd9237930
Sainudheen P <sainudheen2003@gmail.com>
2025-04-21 20:47:29 +0530

    feat: clear local storage on Dashboard component mount

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 const Dashboard = () => {
+  useEffect(() => {
+    localStorage.clear()
+  })
   return <div>dashboard
     <Link to='/login' className="text-amber-300 " > login</Link>
   </div>;
