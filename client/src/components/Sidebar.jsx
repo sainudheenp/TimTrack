77b062920168317d3feb3b95b70fde787504c69c
Sainudheen P <sainudheen2003@gmail.com>
2025-04-23 16:29:02 +0530

    sidebar

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import { useStateContext } from '../context/ContextProvider'
+const Sidebar = () => {
+    const { activeMenu, setActiveMenu, screenSize } = useStateContext()
+
+    const handleSideBarClose = () => { }
+
+    const activeLink =
+        "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+    const normalLink =
+        "flex items-center gap-5 pl-4 pt-3 pb-2.5  rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
+
+
+    return (
+        <div>Sidebar</div>
+    )
+}
+
+export default Sidebar
---
+++ client/src/context/ContextProvider.jsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+
+const initialState = {
+    userProfile: false,
+    notification: false
+}
+
+export const ContextProvider = ({ children }) => {
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState)
+    const [screenSize, setScreenSize] = useState(undefined)
+    const handleClick = (clicked) => {
+        setIsClicked({ ...initialState, [clicked]: true })
+    }
+    return (
+        <StateContext.Provider value={{
+            activeMenu, setActiveMenu, isClicked, setIsClicked, screenSize, setScreenSize, handleClick
+        }}>
+            {children}
+        </StateContext.Provider>
+    )
+}
+export const useStateContext = () => useContext(StateContext)
