65e71fdc80a924f0780743bb68577d27c224ae04
Sainudheen P <sainudheen2003@gmail.com>
2025-04-24 16:21:09 +0530

    feat: improve layout of StartStopControls and Dashboard components for better responsiveness

--- client/src/components/StartStopControls.jsx
+++ client/src/components/StartStopControls.jsx
@@ -2,7 +2,7 @@ import { React, useState } from 'react'
 
 const StartStopControls = () => {
     return (
-        <div className="flex items-center  bg-white p-3 rounded-2xl ">
+        <div className="flex items-center justify-between  bg-white p-3 rounded-2xl ">
             <span className=" font-bold mr-8 text-lg">Start Time Tracker</span>
             <div className=" p-4 px-5 rounded-2xl bg-amber-300 flex items-center justify-center text-2xl">
                 <i className="fa-solid fa-play "></i>
